14fad7ecc99734362a98be0e637d4ddb2813eeed
Spencer Mundel <spencer.mundel@hotmail.com>
2021-10-21 19:58:38 -0700

    Adding onClick logic

--- src/main/java/frc/robot/subsystems/Button.java
+++ src/main/java/frc/robot/subsystems/Button.java
@@ -1,3 +1,12 @@
-public class Button {
-    
+public abstract class Button {
+    public abstract boolean act();
+
+    public void onClick(){
+        if(act()){
+            //DISPLAY SUCCESSFUL BUTTON MESSAGE
+        }
+        else{
+            //DSPLAY FAILURE BUTTON MESSAGE
+        }
+    }
 }
